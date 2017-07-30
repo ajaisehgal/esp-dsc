671b1958e133ca5cf05bd0e078cadca057a17d86
Ajai Sehgal <ajai@sehgal.net>
2017-07-29 22:17:30 -0700

    Minor edit

--- src/esp_dsc/esp_dsc.h
+++ src/esp_dsc/esp_dsc.h
@@ -14,7 +14,6 @@
  */
 
 /* Encoder pins on RJ45 */
-#define CHAN_RA_B   15     // RJ45 pin 1
 #define CHAN_DEC_A   5     // RJ45 pin 7 NodeMCU D1
 #define CHAN_DEC_B   4     // RJ45 pin 5 NodeMCU D2
 #define CHAN_RA_A    2     // RJ45 pin 3 NodeMCU D4
