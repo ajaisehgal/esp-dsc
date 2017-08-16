b80d617d65b2625fea99adc83bef40853d4d0cde
Ajai Sehgal <ajai@sehgal.net>
2017-08-16 12:26:18 -0700

    Fix merge

--- src/esp_dsc/esp_dsc.h
+++ src/esp_dsc/esp_dsc.h
@@ -14,11 +14,14 @@
  */
 
 /* Encoder pins on RJ45 */
-<<<<<<< HEAD
-#define CHAN_DEC_A   5     // NodeMCU D1
-#define CHAN_DEC_B   4     // NodeMCU D2
-#define CHAN_RA_A    12    // NodeMCU D6
-#define CHAN_RA_B    14    // NodeMCU D5
+                           // RJ45 pin 8 - GND
+                           // RJ45 pin 7 - Vcc
+#define CHAN_DEC_A   5     // RJ45 pin 5 NodeMCU D1
+#define CHAN_DEC_B   4     // RJ45 pin 6 NodeMCU D2
+#define CHAN_RA_A   12     // RJ45 pin 1 NodeMCU D6
+#define CHAN_RA_B   14     // RJ45 pin 2 NodeMCU D5
+                           // RJ45 pin 3 - Vcc
+                           // RJ45 pin 4 - GND 
 
 /* Friendly names for the NodeMCU V2 version */
 #define D0 16
@@ -30,18 +33,9 @@
 #define D6 12
 #define D7 13
 #define D8 15
-=======
-                           // RJ45 pin 8 - GND
-                           // RJ45 pin 7 - Vcc
-#define CHAN_DEC_A   5     // RJ45 pin 5 NodeMCU D1
-#define CHAN_DEC_B   4     // RJ45 pin 6 NodeMCU D2
-#define CHAN_RA_A   12     // RJ45 pin 1 NodeMCU D6
-#define CHAN_RA_B   14     // RJ45 pin 2 NodeMCU D5
-                           // RJ45 pin 3 - Vcc
-                           // RJ45 pin 4 - GND 
->>>>>>> 4c4f1204aaa1fbd3196f9446cac826d50154b67d
 
-#define ESP_DSC_VERSION "0.0.2"
+
+#define ESP_DSC_VERSION "0.0.3"
 #define MAX_SRV_CLIENTS 4
 #define MAX_AP_RETRIES 60
 #define SERIAL_SPEED 115200
